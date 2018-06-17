ed7fbfc23891994de8d056175e796fac6ed84062
Natalio Sacerdote <nsacerdote@users.noreply.github.com>
2018-06-17 22:53:33 +0100

    First version of recipe-list, lesson 49 finished

--- angular-course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ angular-course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+        new Recipe('Chicken wings', 'description',
+            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg'),
+        new Recipe('Chicken wings', 'description',
+            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg')
+    ];
 
     constructor() {
     }
