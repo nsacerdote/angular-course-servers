d254d94f6b1015b00a6dc930d7846764d3404121
Natalio Sacerdote (U145070) <natalio.sacerdote@edataconsulting.de>
2018-06-19 16:00:10 +0100

    Lecture 111 finished

--- course-project/src/app/recipe-book/recipe.service.ts
+++ course-project/src/app/recipe-book/recipe.service.ts
@@ -1,16 +1,21 @@
 import {EventEmitter} from '@angular/core';
 
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Chicken wings', 'description',
-            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg'),
-        new Recipe('Chicken', 'description 2',
-            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg')
+        new Recipe('Chicken wings',
+            'description',
+            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg',
+            [new Ingredient('Chicken', 1)]),
+        new Recipe('Chicken',
+            'description 2',
+            'https://assets.epicurious.com/photos/5761c748ff66dde1456dfec0/2:1/w_1260%2Ch_630/crispy-baked-chicken-wings.jpg',
+            [new Ingredient('Chicken', 2)])
     ];
 
     getRecipes() {
